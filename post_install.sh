c024e9e33be30fa2582377e2c97a0f60a8cc6de0
Aljoša Trninić <atrninicc@gmail.com>
2023-09-22 10:07:13 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -26,8 +26,14 @@ sudo systemctl restart sshd
 #Installing Anydesk
 #wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 #echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-#sudo apt update -y
-#sudo apt install anydesk -y
+sudo apt update -y
+sudo apt install anydesk -y
+
+#Installing RustDesk
+wget https://github.com/rustdesk/rustdesk/releases/download/1.2.2/rustdesk-1.2.2-x86_64.deb
+sudo apt install ./rustdesk-1.2.2-x86_64.deb -y
+sudo rm rustdesk-1.2.2-x86_64.deb
+sudo systemctl enable rustdesk.service
 
 #Installing Viber
 #wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
@@ -39,6 +45,17 @@ sudo systemctl restart sshd
 sudo apt update -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
+
+#Fix Mic in Ubuntu install on Logitech H111 headset
+file_path="/etc/modprobe.d/alsa-base.conf"
+line_to_add="options snd-hda-intel model=dell-headset-multi"
+
+if [ -f "$file_path" ]; then
+  echo "$line_to_add" | sudo tee -a "$file_path"
+  echo "Line added to $file_path"
+else
+  echo "File not found: $file_path"
+fi
 
 #Reboot
 sudo reboot
